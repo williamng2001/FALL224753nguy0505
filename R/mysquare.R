e77647b3f87a6b311d9e32fdad7b5f977986dfc2
williamng2001 <willng2001@gmail.com>
2024-09-18 13:41:49 -0500

    first

---
+++ R/mysquare.R
@@ -0,0 +1,12 @@
+#' The Square Function
+#'
+#' @param x a vector of quantitative data
+#'
+#' @return a vector of quanatitative data
+#' @export
+#'
+#' @examples
+#' mysquare(x =1:10)
+mysquare <- function(x){
+  x^2
+}
